877245429445762611383dc9c4f6acff7cc02c3d
nilsmanc <etereaiberia@gmail.com>
2024-01-26 08:33:40 +0300

    useRedirect

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+
+import useRedirectByUserCheck from '@/hooks/useRedirectByUserCheck'
+
+function Dashboard() {
+  const { shouldLoadContent } = useRedirectByUserCheck()
+  const getDefaultTextGenerator = () => ''
+  const getTextGenerator = () => ''
+
+  return (
+    <>
+      <Head>
+        <title>Аква Тепмикс | {shouldLoadContent ? 'Главная' : ''}</title>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/svg" sizes="32x32" href="/img/logo.svg" />
+      </Head>
+      {shouldLoadContent && (
+        <main>
+          <div className="overlay" />
+        </main>
+      )}
+    </>
+  )
+}
+
+export default Dashboard
