1c740a6db4a9a48271e9b12573eaac8ecbd9a7ec
nilsmanc <etereaiberia@gmail.com>
2024-01-27 08:14:25 +0300

    header

---
+++ components/modules/Header/CartPopup/CartPopup.tsx
@@ -0,0 +1,103 @@
+import { useStore } from 'effector-react'
+import { forwardRef, useEffect } from 'react'
+import Link from 'next/link'
+import { toast } from 'react-toastify'
+import { $mode } from '@/context/mode'
+
+import { $user } from '@/context/user'
+import styles from '@/styles/cartPopup/index.module.scss'
+
+const CartPopup = forwardRef<HTMLDivElement>(({ open, setOpen }, ref) => {
+  const mode = useStore($mode)
+  const user = useStore($user)
+  const totalPrice = useStore($totalPrice)
+  const disableCart = useStore($disableCart)
+  const shoppingCart = useStore($shoppingCart)
+  const darkModeClass = mode === 'dark' ? `${styles.dark_mode}` : ''
+
+  const toggleCartDropDown = () => {
+    setOpen(!open)
+  }
+
+  useEffect(() => {
+    loadCartItems()
+  }, [])
+
+  const loadCartItems = async () => {
+    try {
+      const cartItems = await getCartItemsFx(`/shopping-cart/${user.userId}`)
+
+      setShoppingCart(cartItems)
+    } catch (error) {
+      toast.error((error as Error).message)
+    }
+  }
+
+  return (
+    <div className={styles.cart} ref={ref}>
+      {disableCart ? (
+        <button
+          className={`${styles.cart__btn} ${darkModeClass}`}
+          style={{ cursor: 'auto' }}
+        >
+          <span className={styles.cart__svg}></span>
+          <span className={styles.cart__text}>Корзина</span>
+        </button>
+      ) : (
+        <button
+          className={`${styles.cart__btn} ${darkModeClass}`}
+          onClick={toggleCartDropDown}
+        >
+          {!!shoppingCart.length && (
+            <span className={styles.cart__btn__count}>
+              {shoppingCart.length}
+            </span>
+          )}
+          <span className={styles.cart__svg}></span>
+          <span className={styles.cart__text}>Корзина</span>
+        </button>
+      )}
+
+      <h3 className={styles.cart__popup__title}>Корзина</h3>
+      <ul className={styles.cart__popup__list}>
+        {shoppingCart.length ? (
+          shoppingCart.map((item) => (
+            <CartPopupItem key={item.id} item={item} />
+          ))
+        ) : (
+          <li className={styles.cart__popup__empty}>
+            <span
+              className={`${styles.cart__popup__empty__text} ${darkModeClass}`}
+            >
+              Корзина пуста
+            </span>
+          </li>
+        )}
+      </ul>
+      <div className={styles.cart__popup__footer}>
+        <div className={styles.cart__popup__footer__total}>
+          <span
+            className={`${styles.cart__popup__footer__text} ${darkModeClass}`}
+          >
+            Общая сумма заказа:
+          </span>
+          <span className={styles.cart__popup__footer__price}>
+            {formatPrice(totalPrice)} P
+          </span>
+        </div>
+        <Link href="/order" passHref legacyBehavior>
+          <button
+            className={styles.cart__popup__footer__btn}
+            disabled={!shoppingCart.length}
+          >
+            Оформить заказ
+          </button>
+        </Link>
+      </div>
+    </div>
+  )
+})
+
+CartPopup.displayName = 'CartPopup'
+
+export default withClickOutside(CartPopup)
---
+++ components/modules/Header/HeaderBottom.tsx
@@ -0,0 +1,50 @@
+/* eslint-disable @next/next/no-img-element */
+import { useStore } from 'effector-react'
+import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { $mode } from '@/context/mode'
+
+import CartPopup from './CartPopup/CartPopup'
+import styles from '@/styles/header/index.module.scss'
+
+const HeaderBottom = () => {
+  const isMedia950 = useMediaQuery(950)
+  const mode = useStore($mode)
+  const darkModeClass = mode === 'dark' ? `${styles.dark_mode}` : ''
+  const router = useRouter()
+
+  useEffect(() => {
+    if (router.pathname === '/order') {
+      setDisableCart(true)
+      return
+    }
+
+    setDisableCart(false)
+  }, [router.pathname])
+
+  return (
+    <div className={styles.header__bottom}>
+      <div className={`container ${styles.header__bottom__container}`}>
+        <h1 className={styles.header__logo}>
+          <Link href="/dashboard" legacyBehavior passHref>
+            <a className={styles.header__logo__link}>
+              <img src="/img/logo.svg" alt="logo" />
+              <span
+                className={`${styles.header__logo__link__text} ${darkModeClass}`}
+              >
+                Детали для газовых котлов
+              </span>
+            </a>
+          </Link>
+        </h1>
+        <div className={styles.header__search}></div>
+        <div className={styles.header__shopping_cart}>
+          <CartPopup />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeaderBottom
